72bc3fb1c9ca1a7e39839c5303772cdc133744fb
MattH2323 <luomatteo1@gmail.com>
2025-02-10 10:19:08 +0100

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splittedString = string.split("");
+    let reversedString = "";
+    for(let counter = splittedString.length-1; counter >= 0; counter--){
+        reversedString += splittedString[counter];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
