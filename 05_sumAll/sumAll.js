4ab641c6bc550a31c488dbd68b9f240b656cd78c
MattH2323 <luomatteo1@gmail.com>
2025-02-10 13:29:00 +0100

    Complete 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(!Number.isInteger(min) || !Number.isInteger(max)){
+        return 'ERROR';
+    }
+    if((min < 0)||(max < 0)){
+        return 'ERROR';
+    }
+    if(min > max){
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
