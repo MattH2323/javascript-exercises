88c4fd136bb709fc855d58418eb40bf7d3e49cd2
MattH2323 <luomatteo1@gmail.com>
2025-02-10 09:58:48 +0100

    Complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if(times < 0) return 'ERROR';
+    let finalString = "";
+    for(let counter = 0; counter < times; counter++){
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
