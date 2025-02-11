dcc332917d3d369c2a26a108ce34f5a86135ab4f
MattH2323 <luomatteo1@gmail.com>
2025-02-11 09:57:22 +0100

    Complete 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitTemp) {
+  let CelsiusTemp = (FahrenheitTemp - 32)*(5/9);
+  const roundedCelsiusTemp = Math.round(CelsiusTemp*10)/10;
+  return roundedCelsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusTemp) {
+  let FahrenheitTemp = (CelsiusTemp*9/5 + 32);
+  const roundedFahrenheitTemp = Math.round(FahrenheitTemp*10)/10;
+  return roundedFahrenheitTemp;
 };
 
 // Do not edit below this line
